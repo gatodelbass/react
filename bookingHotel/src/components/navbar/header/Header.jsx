a67e7142ed2c38c26da2d31e69ca6f35a6b87f56
gatodelbass <davemmgarcia@gmail.com>
2023-06-30 03:58:43 -0500

    borrar node modules

--- bookingHotel/src/components/navbar/header/Header.jsx
+++ bookingHotel/src/components/navbar/header/Header.jsx
@@ -1,14 +1,37 @@
 import "./header.css"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faBed} from "@fortawesome/free-solid-svg-icons"
+import {faPlane} from "@fortawesome/free-solid-svg-icons"
+import {faCar} from "@fortawesome/free-solid-svg-icons"
+import {faTaxi} from "@fortawesome/free-solid-svg-icons"
 
-  const Navbar = () => {
+  const Header = () => {
   return (
     <div className="header">
         <div className="headerList">
-            <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
+            <div className="headerListItem active">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
             </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport taxis</span>
+            </div>
+            <h1 className="headerTitle">
+                A life of discounts is genius
+            </h1>
+            <p className="headerDescription">get rewader for your travels</p>
+            <button className="headerButton">
+                Sign in / register
+            </button>
         </div>
     </div>
   )
